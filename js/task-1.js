4f6aa0b09da8e132ac47476e73fbd5ff9710eb7b
Dmytr01 <dima1234567890ty@gmail.com>
2023-11-21 18:44:37 +0200

    1

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+function makeTransaction(quantity, pricePreDroid) { 
+    const totalPrice = quantity * pricePreDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500));
